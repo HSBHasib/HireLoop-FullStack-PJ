320a24b92abffbd24ef4dd681857e1eb2698a291
Hasib <hasibhsb19@gmail.com>
2026-06-01 21:47:41 +0600

    add Navbar, footer, banner section file

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,15 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "@/components/HomePage/Navbar/Navbar";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Footer from "@/components/HomePage/Footer/Footer";
 
 export const metadata = {
   title: "Hire Loop",
@@ -18,11 +9,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" data-theme="dark" className={`h-full antialiased`}>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
